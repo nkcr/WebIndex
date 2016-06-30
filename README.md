db3695bb68b36df3c48e0fd2710537eb26ab048e
Noémien Kocher <nkcr.je@gmail.com>
2016-06-30 22:51:35 +0200

    cleaning

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # WebIndex
 Bachelor project
 
-# How to run
+# How to run the webserver
 
 Go to `/app` run `. venv/bin/activate`. Then do `export FLASK_APP=webstart.py` and `flask run`.
 
 If the server needs to be visible outside localhost, run with `flask run --host=0.0.0.0`. To have the debug mode, do `export FLASK_DEBUG=1`.
+
+# Folder structure of the repository
+
+```
+└── app             # Web server
+    ├── data        # Exported data structure
+    ├── lib         # Modules
+    ├── static      # Static web assets
+    │   └── uploads # Uploaded files
+    ├── templates   # HTML view file
+    ├── validation  # Algorithms evaluations
+    └── venv        # Pyhton virtualenv
+```
--- app/webindex.py
+++ app/webindex.py
@@ -46,10 +46,10 @@ class Webindex:
         return self.engine.ii
 
     def saveii(self):
-        iengine.savejson('ii.txt', self.getii())
+        iengine.savejson('data/ii.txt', self.getii())
 
     def saverepo(self):
-        iengine.savejson('repo.txt', self.repo)
+        iengine.savejson('data/repo.txt', self.repo)
 
     def savestats(self, path, quantity=100):
         iengine.settfidf2(self.repo,self.getii())
@@ -60,8 +60,8 @@ class Webindex:
             for k in keys:
                 spamwriter.writerow([k,round(self.getii()[k][0],2)])
 
-webindex = Webindex()
-files = glob.glob('documents/TIF_FR/*.html')
-for file in files:
-    webindex.handlefile(file)
-webindex.savestats('test.csv')
+# webindex = Webindex()
+# files = glob.glob('documents/TIF_FR/*.html')
+# for file in files:
+#     webindex.handlefile(file)
+# webindex.savestats('test.csv')
